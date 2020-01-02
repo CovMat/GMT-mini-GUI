647479343057e5bfd91b313d33156d6ff759584c
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2020-01-02 12:15:31 +0900

    fix bug
    fix bug

--- Run_GMT_CMD.cpp
+++ Run_GMT_CMD.cpp
@@ -9,11 +9,21 @@ Run_GMT_CMD::Run_GMT_CMD(QString S) :
 {}
 
 void Run_GMT_CMD::dowork(){
+    /*
     #if defined(Q_OS_WIN32)
         system(cmd.toUtf8().constData());
     #else
         QProcess::execute(cmd);
     #endif
+    */
+    QString echo_text = cmd.split(' ').at(0);
+    if (echo_text == "echo"){
+        system(cmd.toUtf8().constData());
+    }
+    else{
+        QProcess::execute(cmd);
+    }
+
     /*
     QProcess *pro = new QProcess;
     pro->start(cmd);
